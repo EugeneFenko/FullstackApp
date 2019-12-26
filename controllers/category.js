9798ec0535f9f261f3a7060eeea3ab2c700c4892
gt60 <gt60@ya.ru>
2019-12-27 00:13:05 +0200

    Fullstack app init

---
+++ controllers/category.js
@@ -0,0 +1,28 @@
+module.exports.getAll = function(req,res) {
+  res.status(200).json({
+    message: "getAll"
+  })
+}
+
+module.exports.getByID = function(req,res) {
+  res.status(200).json({
+    message: "getID"
+  })
+}
+
+module.exports.remove = function(req,res) {
+  res.status(200).json({
+    message: "getID"
+  })
+}
+
+module.exports.create = function(req,res) {
+  res.status(200).json({
+    message: "getID"
+  })
+}
+module.exports.update = function(req,res) {
+  res.status(200).json({
+    message: "getID"
+  })
+}
---
+++ controllers/position.js
@@ -0,0 +1,29 @@
+module.exports.getAll = function(req,res) {
+  res.status(200).json({
+    message: "Login from control"
+  })
+}
+
+module.exports.getByID = function(req,res) {
+  res.status(200).json({
+    message: "Login from control"
+  })
+}
+
+module.exports.create = function(req,res) {
+  res.status(200).json({
+    message: "Register from control"
+  })
+}
+
+module.exports.remove = function(req,res) {
+  res.status(200).json({
+    message: "Register from control"
+  })
+}
+
+module.exports.update = function(req,res) {
+  res.status(200).json({
+    message: "Register from control"
+  })
+}
